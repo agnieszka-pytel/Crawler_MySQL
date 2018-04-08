4855f9206474c8868967ff584484910cd3b47b99
Agnieszka Pytel <agnieszkapytel25@gmail.com>
2018-04-08 22:42:14 +0200

    Insert url and anchors into database

--- code/crawler.php
+++ code/crawler.php
@@ -1,40 +1,65 @@
 <?php
-    $pageCrawlerResult = '';
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
 
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $conn->select_db("Crawler");
+
+    $pageCrawlerResult = '';
     function crawl_page($url, $depth = 5)
     {
+        global $conn;
         $result = '';
         static $seen = array();
         if (isset($seen[$url]) || $depth === 0) {
             return;
         }
-
         $seen[$url] = true;
-
         $dom = new DOMDocument('1.0');
         @$dom->loadHTMLFile($url);
 
         $anchors = $dom->getElementsByTagName('a');
         $hrefArray = [];
-
         foreach ($anchors as $element) {
-            $href = $element->getAttribute('href');
-
-            $hrefArray[] = $href;
+            // Remove anchors
+            $finalLink = explode("#", $element->getAttribute('href'));
+            $link = $finalLink[0];
+            // Add the protocol
+            $adres = substr($link, 0, 7);
+            $adresS = substr($link, 0, 8);
+            $protocol = 'http://';
+            $protocolS = 'https://';
+            if($adres != $protocol && $adresS != $protocolS){
+                //echo '<br>Brak protokolu<br><br>';
+                $link = $url.$link;
+            }
+            // Push final link to array
+            $hrefArray[] = $link;
         }
         $hrefArray = array_unique($hrefArray);
+        $anchors_text = implode(',',$hrefArray);
+
+        $sql = "INSERT INTO SitesAwaiting(site,content) VALUES ('{$url}', '{$anchors_text}')";
+        if (mysqli_query($conn, $sql)) {
+            echo '<p>'."Inserted values into SitesAwaiting successfully".'</p>';
+        } else {
+            echo '<p>'."Error inserting values: " . mysqli_error($conn).'</p>';
+        }
 
         foreach ($hrefArray as $href) {
             $result .= '<a href="' . $href . '">' . $href . '</a>';
         }
-
         return $result;
     }
-
     if(!empty($_GET['url'])){
         $url = $_GET['url'];
     }
-
     if(isset($url)) {
         if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
             echo 'Not a valid html!!!';
@@ -42,6 +67,9 @@
             $pageCrawlerResult = crawl_page($url, 2);
         }
     }
+
+    $conn->close();
+
 ?>
 
 <!DOCTYPE html>
@@ -55,7 +83,7 @@
   </head>
   <body>
     <div class="header">Crawler</div>
-  	<div class="search">
+    <div class="search">
         <form action="" type="GET">
             <div class="search-container">
                 <input type="text" class="search-input" name="url" value="<?php if(!empty($_GET['url'])){ echo $url; } ?>">
@@ -64,7 +92,7 @@
                 <input class="submit" type="submit" value="Crawl!">
             </div>
         </form>
-  	</div>
+    </div>
     <div class="result">
         <?php
             echo $pageCrawlerResult;
--- code/create_db.php
+++ code/create_db.php
@@ -1,52 +1,80 @@
 <?php
-$servername = "mysql";
+$servername = "localhost";
 $username = "root";
-$password = "mysql";
-
+$password = "";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 // Create database
 $sql = "CREATE DATABASE IF NOT EXISTS Crawler";
 if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
+    echo '<p>'."Database created successfully".'</p>';
 } else {
-    echo "Error creating database: " . $conn->error;
+    echo '<p>'."Error creating database: " . $conn->error.'</p>';
 }
-
 $conn->select_db("Crawler");
-
 // sql to create table
 $sql = "CREATE TABLE IF NOT EXISTS SitesViewed (
-	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	site TEXT,
-	date TIMESTAMP
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    site TEXT,
+    date TIMESTAMP
 )";
-
 if (mysqli_query($conn, $sql)) {
-    echo "Table SitesViewed created successfully";
+    echo '<p>'."Table SitesViewed created successfully".'</p>';
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo '<p>'."Error creating table: " . mysqli_error($conn).'</p>';
 }
-
 // sql to create table
 $sql = "CREATE TABLE IF NOT EXISTS SitesAwaiting (
-	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	site VARCHAR(200),
-	date TIMESTAMP
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    site VARCHAR(200),
+    date TIMESTAMP
 )";
-
 if (mysqli_query($conn, $sql)) {
-    echo "Table SitesAwaiting created successfully";
+    echo '<p>'."Table SitesAwaiting created successfully".'</p>';
 } else {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo '<p>'."Error creating table: " . mysqli_error($conn).'</p>';
 }
-
-
+$result = mysqli_query($conn, "SHOW COLUMNS FROM `SitesAwaiting` LIKE 'content'");
+$exists = (mysqli_num_rows($result)) ? TRUE:FALSE;
+if (!$exists) {
+    // sql to ALTER table
+    $sql = "ALTER TABLE SitesAwaiting ADD links TEXT after site";
+    if (mysqli_query($conn, $sql)) {
+        echo '<p>'."ALTER TABLE SitesAwaiting successfully".'</p>';
+    } else {
+        echo '<p>'."Error creating table: " . mysqli_error($conn).'</p>';
+    }
+    
+    // sql to ALTER table
+    $sql = "ALTER TABLE SitesAwaiting MODIFY site VARCHAR(2048)";
+    if (mysqli_query($conn, $sql)) {
+        echo '<p>'."ALTER TABLE SitesAwaiting successfully".'</p>';
+    } else {
+        echo '<p>'."Error creating table: " . mysqli_error($conn).'</p>';
+    }
+}
+$result = mysqli_query($conn, "SHOW COLUMNS FROM `SitesViewed` LIKE 'content'");
+$exists = (mysqli_num_rows($result)) ? TRUE:FALSE;
+if (!$exists) {
+    // sql to ALTER table
+    $sql = "ALTER TABLE SitesViewed ADD links TEXT after site";
+    if (mysqli_query($conn, $sql)) {
+        echo '<p>'."ALTER TABLE SitesViewed successfully".'</p>';
+    } else {
+        echo '<p>'."Error creating table: " . mysqli_error($conn).'</p>';
+    }
+    
+    // sql to ALTER table
+    $sql = "ALTER TABLE SitesViewed MODIFY site VARCHAR(2048)";
+    if (mysqli_query($conn, $sql)) {
+        echo '<p>'."ALTER TABLE SitesViewed successfully".'</p>';
+    } else {
+        echo '<p>'."Error creating table: " . mysqli_error($conn).'</p>';
+    }
+}
 $conn->close();
-
 ?>
